bd486b6f400e2f7ed564ff73f114e2e8b12d8b15
妍嘉 程 <107111213@mail.oit.edu.tw>
2021-04-13 14:56:34 +0800

    3-1

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,28 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            int i_Year = 1000;
+            bool b_IsLeap = false;
 
+            if (i_Year %  4 == 0){
+                b_IsLeap = true;
+
+                if (i_Year % 100 == 0){
+                    if(i_Year % 400 == 0) { 
+                    b_IsLeap = true;
+                }
+                else{
+                    b_IsLeap = false;
+                }
+            }
+        }
+        if(b_IsLeap == true){
+           Response.Write("Y");
+        }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
---
+++ 2-2Practice/GenerateBomb.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_2Practice
+{
+    public partial class GenerateBomb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] ia_Bomb = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
+            int i_Row = 0, i_Col = 0;
+            int[,] ia_2DMap = new int[10, 10];
+
+            for(int i_Ct = 0; i_Ct < ia_Bomb.Length; i_Ct++)
+            {
+                mt_GetRowCol(ref i_Row, ref i_Col, ia_Bomb[i_Ct]);
+                ia_2DMap[i_Row, i_Col] = -1;
+            }
+            for(int i_Ct = 0; i_Ct < 10; i_Ct++)
+            {
+                for(int i_Ct2 = 0; i_Ct2 < 10; i_Ct2++)
+                {
+                    Response.Write(ia_2DMap[i_Ct, i_Ct2]);
+                }
+                Response.Write("<br />");
+            }
+        }
+
+        private void mt_GetRowCol(ref int i_Row, ref int i_Col, int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
